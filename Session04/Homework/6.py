1409238d8d4fa949e1dfc39f9c5f0200d87b149d
Le Ha Thu <lhathu123@gmail.com>
2018-04-21 00:10:58 +0700

    initial commit

--- Session04/Homework/6.py
+++ Session04/Homework/6.py
@@ -1,4 +1,4 @@
-class print():
+class Print_str:
     string="abc"
 
     def get_string(self,g):
@@ -7,6 +7,6 @@ class print():
     def print_string(self):
         return print(self.string.upper())
 
-str1=print()
-str1.get_string(input())
+str1=Print_str()
+str1.get_string(input("enter string: "))
 str1.print_string()
