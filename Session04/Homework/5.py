ae1367f3d7ed003c5d8a305f04e5f39a50eb1ed5
Le Ha Thu <lhathu123@gmail.com>
2018-04-20 23:54:38 +0700

    initial commit

--- Session04/Homework/5.py
+++ Session04/Homework/5.py
@@ -6,7 +6,7 @@ def selection_sort(list):
             if list[min]>list[j]:
                 min=j
         if min!=i:
-            list[j],list[min]=list[min],list[j]
+            list[i],list[min]=list[min],list[i]
     return list
 
 print(selection_sort([4,8,1,3,2,9,5,7,6]))
---
+++ Session04/Homework/6.py
@@ -0,0 +1,12 @@
+class print():
+    string="abc"
+
+    def get_string(self,g):
+        self.string=g
+
+    def print_string(self):
+        return print(self.string.upper())
+
+str1=print()
+str1.get_string(input())
+str1.print_string()
